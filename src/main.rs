ca834d7cbf0ba02440c91898e765c103c5dfd926
emile <emile@guyotfamily.com>
2025-01-18 23:00:29 +0100

    Setup the debug branch.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use leptos::prelude::*;
 
 fn main() {
+    console_error_panic_hook::set_once();
+
     leptos::mount::mount_to_body(|| {
-        view! {
-            <p>"Hello, World!"</p>
-        }
+        view! { <p>"Hello, World!"</p> }
     });
 }
